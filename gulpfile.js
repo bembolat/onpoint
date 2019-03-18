64bc4649b2764ccea30ffb1cb25f2fa69d48c033
bembolat <barkadwood@gmail.com>
2019-03-18 20:32:22 +0300

    connection min css

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,8 @@ gulp.task("clean", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-      "source/fonts/**/*.{woff,woff2}"
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/*.{png,jpg}"
     ], {
       base: "source"
     })
